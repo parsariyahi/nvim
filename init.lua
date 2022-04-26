011f51659f7131ce2d0141884d6729d516597ea6
parsariyahi <pany.parsariyahi@gmail.com>
2022-04-26 20:57:48 +0430

    moved from vim to lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+require "parsa.options"
+require "parsa.keymaps"
+require "parsa.plugins"
+require "parsa.colorscheme"
+require "parsa.cmp"
+require "parsa.lsp"
+require "parsa.treesitter"
+require "parsa.autopairs"
