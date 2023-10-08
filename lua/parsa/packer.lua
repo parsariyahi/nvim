bbe75effb6a2928f6face913be6ecf713352f19b
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-08 12:38:02 +0330

    add ``treesitter`` and ``treesitter playground`` package and configurations

--- lua/parsa/packer.lua
+++ lua/parsa/packer.lua
@@ -15,5 +15,9 @@ return require('packer').startup(function(use)
 
     use({ 'rose-pine/neovim', as = 'rose-pine' })
 
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/playground')
+    use("theprimeagen/harpoon")
+
 
 end)
