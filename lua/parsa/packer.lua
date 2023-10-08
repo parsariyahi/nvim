603a3d4fec3a7086c92812adcffe37c3340f83ac
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-08 12:48:31 +0330

    add ``vim-fugitive`` package and configurations

--- lua/parsa/packer.lua
+++ lua/parsa/packer.lua
@@ -19,5 +19,6 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
 end)
