3b74b33263e097ce151c9372bc446174996bf619
parsariyahi <parsa.parsariyahi@gmail.com>
2025-04-12 16:48:28 +0330

    add telescope page up and down in pop up

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,20 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup({
+  defaults = {
+    mappings = {
+      n = {
+        ["<C-u>"] = require('telescope.actions').preview_scrolling_up,
+        ["<C-d>"] = require('telescope.actions').preview_scrolling_down,
+      },
+      i = {
+        ["<C-u>"] = require('telescope.actions').preview_scrolling_up,
+        ["<C-d>"] = require('telescope.actions').preview_scrolling_down,
+      },
+    },
+  },
+})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
